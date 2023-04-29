879c962853ae9e413b2994feeba29602e3d3155b
mehmet ali sezgin <49106964+mehmetalisezgin@users.noreply.github.com>
2023-04-29 20:12:53 +0200

    practice

---
+++ src/test/java/week01/Class02_DriverMethods.java
@@ -0,0 +1,19 @@
+package week01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class02_DriverMethods {
+
+    public static void main(String[] args) {
+
+        WebDriver chromeDriver = new ChromeDriver() ;
+        chromeDriver.get("https://www.amazon.com/");
+
+
+
+
+
+
+    }
+}
